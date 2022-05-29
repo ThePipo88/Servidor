b9927f78ef8d64b61849e9aac42312738ffeea4f
Freddy <freddygm_16@hotmail.com>
2022-05-28 20:51:39 -0600

    Consultas http
    asd

--- src/routers/casos.js
+++ src/routers/casos.js
@@ -3,10 +3,10 @@ const CasosController = require("../controller/casos");
 
 const api = express.Router();
 
-api.post("/registrar/usuarios", CasosController.registrar);
-api.get("/obtener/:id", CasosController.findById);
-api.get("/obtener/", CasosController.findAll);
-api.put("/actualizar/:id", CasosController.updateCaso);
-api.delete("/eliminar/:id", CasosController.deleteCaso);
+api.post("/registrar/casos", CasosController.registrar);
+api.get("/obtener/caso/:id", CasosController.findById);
+api.get("/obtener/caso", CasosController.findAll);
+api.put("/actualizar/caso/:id", CasosController.updateCaso);
+api.delete("/eliminar/caso/:id", CasosController.deleteCaso);
 
 module.exports = api;
